9f3b49f772c65999a17cf77f59a8dd798daf6153
AbdullahMouzeen <abdullah.mouazeen@gmail.com>
2020-12-04 18:00:32 +0200

    update files

--- math_series/series.py
+++ math_series/series.py
@@ -1,3 +1,6 @@
+"""
+this finction return the nth value in the fibonacci series.(using recursion)
+"""
 def fibonacci(n):
     if n==0:
         return 0
@@ -5,7 +8,9 @@ def fibonacci(n):
         return 1    
     return  fibonacci(n-1)+fibonacci(n-2)
 
-
+"""
+this finction return the nth value in the lucas series.(using recursion)
+"""
 def lucas(n):
        if n==0:
         return 2
@@ -14,11 +19,23 @@ def lucas(n):
        return  lucas(n-1)+lucas(n-2)
 
 
+"""
+this function take three params 
+the first one  will determine which element in the series to print
+the other params will have default values of 0 and 1 and will determine the first two values for the series to be produced.
 
-def sum_series(n,para2,para3):
+"""
+def sum_series(n,para2=0,para3=1):
      if n==0:
         return para2
      if n==1:
         return para3   
      return  sum_series(n-1,para2,para3)+sum_series(n-2,para2,para3)
 
+
+"""
+outputs
+"""
+print(fibonacci(7))
+print(lucas(7))
+print(sum_series(7,2,1))
--- tests/test_series.py
+++ tests/test_series.py
@@ -1,20 +1,34 @@
-from math_series.series import fibonacci
-from math_series.series import lucas
-from math_series.series import sum_series
+from math_series.series import *
 
-
-def test_any():
+def test_fibonacci():
     actual=fibonacci(3)
     expected=2
     assert actual==expected
     
-def test_any1():
+def test_fibonacci1():
+    actual=fibonacci(4)
+    expected=3
+    assert actual==expected
+
+def test_lucas():
     actual=lucas(3)
     expected=4
     assert actual==expected
 
 
-def test_any2():
-    actual=sum_series(3,0,1)
+def test_lucas1():
+    actual=lucas(4)
+    expected=7
+    assert actual==expected
+
+
+def test_sum_series():
+    actual=sum_series(3)
     expected=2
+    assert actual==expected    
+
+    
+def test_sum_series1():
+    actual=sum_series(3,2,1)
+    expected=4
     assert actual==expected    
